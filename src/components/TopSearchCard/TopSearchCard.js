c15195ce912ebd080a1b249b6ce37c033f96eb1e
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-03 16:18:42 +0530

    topolaces.json file fetch in js file

---
+++ src/components/TopSearchCard/TopSearchCard.js
@@ -0,0 +1,21 @@
+import './TopSearchCard.css'
+
+const TopSearchCard=({name,img1,img2,img3,description,history})=>{
+    
+    return(
+        <div className="topsearchcard">
+            <div className=' img-group'>
+                <img src={img1} className='img'/>
+                <img src={img2} className='img'/>
+                <img src={img3} className='img'/>
+            </div>
+            <div className='name'>{name}</div>
+            <h1 className='heading'>description</h1>
+            <div className='description'>{description}</div>
+            <h1 className='heading'>history</h1>
+            <div className='description'>{history}</div>
+
+        </div>
+    )
+}
+export default TopSearchCard;
--- src/views/TopSearchPlaces/TopSearchPlaces.js
+++ src/views/TopSearchPlaces/TopSearchPlaces.js
@@ -1,11 +1,38 @@
+import TopSearchCard from '../../components/TopSearchCard/TopSearchCard';
+import tosearchdata from './../../CityData/topplaces.json'
+import React, { useState,useEffect } from "react";
+import './TopSearchPlaces.css'
+
 const TopSearchPlaces =() => {
+    const [topsearch,setTopsearch]=useState(tosearchdata.topsearch);
+    const [searchterm, setSearchterm] = useState("");
+
+    useEffect(() => {
+    const filtereddata=tosearchdata.topsearch.filter((topsearchplace)=>{
+        const {placename}=topsearchplace;
+        const lowerplacename=placename.toLowerCase()
+        return (lowerplacename.includes(searchterm))
+    })
+    setTopsearch(filtereddata);
+},[searchterm]);
+
+
     return(
         <>
-         <h1>This is top search places  page</h1>
-         </>
-       
+        <h1>Top Search Places</h1>
+        <input type='text'
+          placeholder= ' Search'
+          value={searchterm}
+          onChange={(e) => { setSearchterm
+            (e.target.value) }}
+        />
+        {
+            topsearch.map((elementofsearch,i)=>(
+             <TopSearchCard name={elementofsearch.placename} img1={elementofsearch.placeimg[0]} img2={elementofsearch.placeimg[1]} img3={elementofsearch.placeimg[2]} description={elementofsearch.placedescription} history={elementofsearch.placehistory}/>
 
-    )
-
+            ))
+        }
+        </>
+    );
 }
 export default TopSearchPlaces;
