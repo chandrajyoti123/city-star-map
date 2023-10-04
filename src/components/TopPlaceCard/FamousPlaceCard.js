2261aeb228abc3b1c150d480c568854108d03c5e
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-04 16:31:03 +0530

    FamousPlaceCards render in js file

--- src/components/TopPlaceCard/FamousPlaceCard.js
+++ src/components/TopPlaceCard/FamousPlaceCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './FamousPlaceCard.css'
 import {breakString} from '../../utils/string'
 
-export default function FamousPlaceCard({image, title, description}){
+export default function FamousPlaceCard({image, title, description,no }){
     return(
         <div className="topplacecard-container">
             <div>
                 <img className='toppalaceimage' src={image} alt=""/>
             </div>
-            <p className='titleplace'><span className='number-box'>1</span>{title}</p>
+            <p className='titleplace'><span className='number-box'>{no}</span>{title}</p>
             <p className='description-place'>{breakString(description, 175 )}{'...'}</p>    
         </div>
     )
--- src/views/TopSearchPlaces/TopSearchPlaces.js
+++ src/views/TopSearchPlaces/TopSearchPlaces.js
@@ -1,46 +1,53 @@
 import TopSearchCard from '../../components/TopSearchCard/TopSearchCard';
 import tosearchdata from './../../CityData/topplaces.json'
-import React, { useState,useEffect } from "react";
-import img from './section5.jpg'
+import React, { useState, useEffect } from "react"; 
 import './TopSearchPlaces.css'
-import TopPlaceCard from '../../components/TopPlaceCard/FamousPlaceCard';
 import Navbar from '../../components/Navbar/Navbar';
 import FamousPlaceCard from '../../components/TopPlaceCard/FamousPlaceCard';
 
-const TopSearchPlaces =() => {
-    const [topsearch,setTopsearch]=useState(tosearchdata.topsearch);
+const TopSearchPlaces = () => {
+    const [topsearch, setTopsearch] = useState(tosearchdata.topsearch);
     const [searchterm, setSearchterm] = useState("");
 
     useEffect(() => {
-    const filtereddata=tosearchdata.topsearch.filter((topsearchplace)=>{
-        const {placename}=topsearchplace;
-        const lowerplacename=placename.toLowerCase()
-        return (lowerplacename.includes(searchterm))
-    })
-    setTopsearch(filtereddata);
-},[searchterm]);
+        const filtereddata = tosearchdata.topsearch.filter((topsearchplace) => {
+            const { placename } = topsearchplace;
+            const lowerplacename = placename.toLowerCase()
+            return (lowerplacename.includes(searchterm))
+        })
+        setTopsearch(filtereddata);
+    }, [searchterm]);
 
-    return(
+    const [place, setPlace] =useState(tosearchdata.topsearch)
+
+    return (
         <>
-        <Navbar/>
-        <p className='main-heading'>Top Tourist Places Attraction In India</p>
-        < input type='text'
-          placeholder= ' Search'
-          value={searchterm}
-          onChange={(e) => { setSearchterm
-            (e.target.value) }}
-        />
-    
-        {/* {
+            <Navbar />
+            <p className='main-heading'>Top Tourist Places Attraction In India</p>
+            < input type='text'
+                placeholder=' Search'
+                value={searchterm}
+                onChange={(e) => {
+                    setSearchterm
+                        (e.target.value)
+                }}
+            />
+
+            {/* {
             topsearch.map((elementofsearch,i)=>(
              <TopSearchCard name={elementofsearch.placename} img1={elementofsearch.placeimg[0]} img2={elementofsearch.placeimg[1]} img3={elementofsearch.placeimg[2]} description={elementofsearch.placedescription} history={elementofsearch.placehistory}/>
 
             ))
         } */}
-        <div>
-            
-        </div>
-        <FamousPlaceCard image={img} title={"India Gate, Delhi"} description={"Located on Rajpath in the heart of Delhi, this arch monument is a 42 meter high war memorial. It is also surrounded by vast well-manicured lawns and many government buildings.hghjjjkkkmmmmmmkkjhggfghjjmkk,,,,,,,,,,,,,,,,,,ghjjkkk,,,,"}/>
+
+            <div className="topsearch-container">
+                {
+                    place.map((placecitydata,i) => {
+                        // const {placename ,placeimg,  placedescription } = placecitydata;
+                        return <FamousPlaceCard image={placecitydata.placeimg[0]}  title={placecitydata.placename} description={placecitydata.placedescription} no={i+1} />
+                    })
+                }
+            </div>
         </>
     );
 }
