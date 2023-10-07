99ebaebdee4c1a631411427eece3e52997b7f65c
harshvardhan khairnar <harshvardhan@kali.kali.org>
2023-10-07 17:29:26 +0530

    blogCards modified && image added

--- src/components/BlogCard/BolgCard.js
+++ src/components/BlogCard/BolgCard.js
@@ -1,14 +1,19 @@
 import blogData from "./../../CityData/blog-data.json";
+import "./BlogCard.css";
 
 
-const BlogCard = ({id, title,Author,Description,Content}) => {
+const BlogCard = ({id,image, title,Author,Description,Content,Publish}) => {
   return (
     <>
-        <div>
-          <h1>{title}</h1>
-          <span>{Author}</span>
+    
+        <div className="blogCard">
+          <div className="blog-img">
+          <img className="blog-img-content" src={image} alt="image"/>
+          </div>
+          <p>{title}</p>
           <p>{Description}</p>
           <p>{Content}</p>
+          <p>{Publish}</p>
         </div>
 
     </>
--- src/views/Blogs/Blogs.js
+++ src/views/Blogs/Blogs.js
@@ -1,18 +1,30 @@
 import Navbar from "../../components/Navbar/Navbar";
-import blogData from "./../../CityData/blog-data.json"
+import blogData from "./../../CityData/blog-data.json";
 import BlogCard from "../../components/BlogCard/BolgCard";
 import Footer from "./../../components/Footer/Footer";
+import "./Blogs.css"
 
 const Blogs = () => {
   return (
     <>
       <Navbar />
-      {
-        blogData.map((blog, index) => {
-            return <BlogCard key={index} id={blog.id} title={blog.title} Author={blog.Author} Description={blog.Description} Content={blog.Content} />
-        })
-      }
-      <Footer/>
+      <div className=".blogCards-container">
+        {blogData.map((blog, index) => {
+          return (
+            <BlogCard
+              key={index}
+              id={blog.id}
+              image={blog.image}
+              title={blog.title}
+              Author={blog.Author}
+              Description={blog.Description}
+              Content={blog.Content}
+              Publish={blog.Publish}
+            />
+          );
+        })}
+      </div>
+      <Footer />
     </>
   );
 };
