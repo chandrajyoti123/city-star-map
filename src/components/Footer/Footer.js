6b678492d606779cd1c16a325ee75f04e09491ee
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-10-07 11:51:00 +0530

    Readme introduction about project added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,42 +2,20 @@ import React from 'react';
 import { Link } from "react-router-dom"
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import{faHouse,faEnvelope,faPhone,faInstagram}  from '@fortawesome/free-solid-svg-icons';
+import{faHouse,faEnvelope,faPhone}  from '@fortawesome/free-solid-svg-icons';
+import{faInstagram,faGithub,faFacebook,faLinkedin,}  from '@fortawesome/free-brands-svg-icons';
 
   const Footer= () =>
   {
     return(
       <>
 
- <footer class="text-center text-lg-start bg-blue text-light mt-5">
-  <section class="d-flex justify-content-center justify-content-lg-between p-2 border-bottom">
-  
-    {/* <div class="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
-    </div> 
-
-    <div>
-      <a href="https://www.facebook.com" class="me-4 text-reset">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-linkedin"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
-      </a>
-    </div> */}
-   
-  </section>
+ <footer class="text-center text-lg-start bg-blue text-light p-2 mt-5">
+    {/* <section class="d-flex justify-content-center justify-content-lg-between p-2 border-bottom">
+    
+    
+    
+    </section> */}
    
   <section class="">
     <div class="container text-center text-md-start mt-5">
@@ -46,9 +24,9 @@ import{faHouse,faEnvelope,faPhone,faInstagram}  from '@fortawesome/free-solid-sv
        
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
        
-          <h6 class="text-uppercase fw-bold mb-4 fs-4">
+          <p class="text-uppercase fw-bold mb-4 fs-4">
             City star Map
-          </h6>
+          </p>
           <p class="fs-5">
           The "City Star Map" project is a modern web application designed to help users explore and learn more about cities around the world.
           </p>
@@ -56,9 +34,9 @@ import{faHouse,faEnvelope,faPhone,faInstagram}  from '@fortawesome/free-solid-sv
         
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
         
-          <h6 class="text-uppercase fw-bold mb-4 fs-4">
+          <p class="text-uppercase fw-bold mb-4 fs-4">
             Quick Links
-          </h6>
+          </p>
           <p class="fs-fs-5" >
             <Link class="text-reset" to={"/"}>Home </Link>
             {/* <a href="#!" class="text-reset">Review</a> */}
@@ -78,9 +56,9 @@ import{faHouse,faEnvelope,faPhone,faInstagram}  from '@fortawesome/free-solid-sv
       
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
          
-          <h6 class="text-uppercase fw-bold mb-4 fs-4">
+          <p class="text-uppercase fw-bold mb-4 fs-4">
             Useful links
-          </h6>
+          </p>
           <p class="fs-fs-5">
             <a href="#!" class="text-reset">Pricing</a>
           </p>
@@ -98,12 +76,18 @@ import{faHouse,faEnvelope,faPhone,faInstagram}  from '@fortawesome/free-solid-sv
 
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
          
-          <h6 class="text-uppercase fw-bold mb-4 fs-4">Contact</h6>
-          <p class="fs-fs-5"> <FontAwesomeIcon icon={faHouse} /> 404, Laxmi Heights, shevalevadi, pune - 412307
+          <p class="text-uppercase fw-bold mb-4 fs-4">Contact</p>
+          <p class="fs-fs-5"> <FontAwesomeIcon icon={faHouse} /> 403, Laxmi Heights, shevalevadi, pune - 412307
           </p>
           <p  class="fs-fs-5"><FontAwesomeIcon icon={faEnvelope}/> sonaliygadekar@gmail.com</p>
           <p class="fs-fs-5"><FontAwesomeIcon icon={faPhone}/> + 91 9767973193</p>
-          {/* <p> <FontAwesomeIcon icon={faInstagram} /></p> */}
+          <p  class="fs-5 ">
+          <a href="https://github.com/sonaligadekar20" target="_blank" class="me-4 text-reset "> <FontAwesomeIcon icon={faGithub} /></a>
+          <a href="" class="me-4 text-reset"> <FontAwesomeIcon icon={faFacebook} /></a>
+          <a href="https://www.instagram.com/sonaliygadekar/"target="_blank" class="me-4 text-reset"> <FontAwesomeIcon icon={faInstagram} /></a>
+          <a href="https://www.linkedin.com/in/sonali-gadekar/" target="_blank" class="me-4 text-reset">  <FontAwesomeIcon icon={faLinkedin} /></a>        
+          </p>
+        
         </div>
     
       </div>
